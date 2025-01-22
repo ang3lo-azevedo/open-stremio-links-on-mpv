648d66abd531eaeae20fe0690510a175ebe5244b
Ângelo Azevedo <ang3lo.azevedo@proton.me>
2025-01-22 18:40:54 +0000

    Update version to 2.2 in open-stremio-links-on-mpv.user.js; reflect recent improvements in link processing functionality.

--- open-stremio-links-on-mpv.user.js
+++ open-stremio-links-on-mpv.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name           Open Stremio Links on MPV
-// @version        2.1
+// @version        2.2
 // @description    Replaces the links when the option "M3U Playlist" is active and opens them on MPV
 // @author         Ângelo Azevedo
 // @match          *://web.stremio.com/*
